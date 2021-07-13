650ecb142cf401d6d75cf6f9d6a21d4b9c331553
Boris Dayma <boris.dayma@gmail.com>
2021-07-13 14:49:56 -0500

    feat: update script

--- seq2seq/do_run.sh
+++ seq2seq/do_run.sh
@@ -3,9 +3,14 @@ python run_seq2seq_flax.py \
 	--train_file /data/CC12M/encoded-small-train.tsv \
 	--validation_file /data/CC12M/encoded-small-valid.tsv \
 	--output_dir output \
-	--per_device_train_batch_size 24 \
-	--per_device_eval_batch_size 24 \
-	--preprocessing_num_workers 48 \
+	--per_device_train_batch_size 56 \
+	--per_device_eval_batch_size 56 \
+	--preprocessing_num_workers 80 \
 	--warmup_steps 1000 \
+	--gradient_accumulation_steps 8 \
 	--do_train \
 	--do_eval \
+	--adafactor \
+	--num_train_epochs 1 \
+	--max_train_samples 200000 \
+	--learning_rate
