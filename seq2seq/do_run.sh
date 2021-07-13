b66b9510cecdc344626626ec9ed2cb48de437240
Boris Dayma <boris.dayma@gmail.com>
2021-07-13 11:20:38 -0500

    feat: default to 1000 warmup steps

--- seq2seq/do_run.sh
+++ seq2seq/do_run.sh
@@ -6,5 +6,6 @@ python run_seq2seq_flax.py \
 	--per_device_train_batch_size 24 \
 	--per_device_eval_batch_size 24 \
 	--preprocessing_num_workers 48 \
+	--warmup_steps 1000 \
 	--do_train \
 	--do_eval \
