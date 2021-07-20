fa14435e4bbedc1507ecebb125a1500f1374b098
Boris Dayma <boris.dayma@gmail.com>
2021-07-20 02:03:41 -0500

    feat: allow display in multi lines

--- dalle_mini/helpers.py
+++ dalle_mini/helpers.py
@@ -1,11 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def captioned_strip(images, caption):
+def captioned_strip(images, caption=None, rows=1):
     increased_h = 0 if caption is None else 48
     w, h = images[0].size[0], images[0].size[1]
-    img = Image.new("RGB", (len(images)*w, h + increased_h))
+    img = Image.new("RGB", (len(images)*w//rows, h*rows + increased_h))
     for i, img_ in enumerate(images):
-        img.paste(img_, (i*w, increased_h))
+        img.paste(img_, (i//rows*w, increased_h + (i % rows) * h))
 
     if caption is not None:
         draw = ImageDraw.Draw(img)
