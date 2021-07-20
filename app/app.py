9f85e8c8da9bdfa05aa2d0cadecabf7362db7a7b
Pedro Cuenca <pedro@latenitesoft.com>
2021-07-20 19:58:11 +0200

    Reuse text field for the various messages. Change some strings. Do not give details about BACKEND_SERVER unless in DEBUG mode.
    Former-commit-id: f4760718fae25053b42fe0d47906b667ceb33e0f

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@
 
 import random
 from dalle_mini.backend import ServiceError, get_images_from_backend
-from dalle_mini.helpers import captioned_strip
 
 import streamlit as st
 
@@ -12,24 +11,26 @@ st.sidebar.title("DALL-E Mini")
 sc = st.sidebar.beta_columns(2)
 sc[0].image('../img/logo.png', width=150)
 sc[1].write("  ")
-sc[1].markdown("Generate images from a text prompt")
+sc[1].markdown("Generate images from text")
 st.sidebar.markdown("""
 ##### Dall-E Mini
 ___
-Dall-E Mini is an AI model that generates images of your prompt!
+Dall-E Mini is an AI model that generates images from any prompt you give it!
 
 Created by Boris Dayma et al. 2021 | [GitHub](https://github.com/borisdayma/dalle-mini) | See [Report](https://wandb.ai/dalle-mini/dalle-mini/reports/DALL-E-mini--Vmlldzo4NjIxODA)
         """)
 
 st.header('DALL-E mini Demo')
-st.subheader('Generate images from a text prompt')
+st.subheader('Generate images from text')
 
 prompt = st.text_input("What do you want to see?")
 
 #TODO: I think there's an issue where we can't run twice the same inference (not due to caching) - may need to use st.form
 
+DEBUG = False
 if prompt != "":
-    st.write(f"Generating candidates for: {prompt}")
+    container = st.empty()
+    container.markdown(f"Generating predictions for: **{prompt}**")
 
     try:
         backend_url = st.secrets["BACKEND_SERVER"]
@@ -39,15 +40,20 @@ if prompt != "":
         cols = st.beta_columns(4)
         for i, img in enumerate(selected):
             cols[i%4].image(img)
+
+        container.markdown(f"**{prompt}**")
         
     except ServiceError as error:
-        st.write(f"Service unavailable, status: {error.status_code}")
+        container.text(f"Service unavailable, status: {error.status_code}")
     except KeyError:
-        st.write("""
-        **Error: BACKEND_SERVER unset**
+        if DEBUG:
+            container.markdown("""
+            **Error: BACKEND_SERVER unset**
 
-        Please, create a file called `.streamlit/secrets.toml` inside the app's folder and include a line to configure the server URL:
-        ```
-        BACKEND_SERVER="<server url>"
-        ```
-        """)
+            Please, create a file called `.streamlit/secrets.toml` inside the app's folder and include a line to configure the server URL:
+            ```
+            BACKEND_SERVER="<server url>"
+            ```
+            """)
+        else:
+            container.markdown('Error -5, please try again or [report it](mailto:pcuenca-dalle@guenever.net).')
