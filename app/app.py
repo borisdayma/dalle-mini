74974be28e70418a359f6b8c6963ca0e7e0eb36a
Boris Dayma <boris.dayma@gmail.com>
2021-11-28 23:06:59 +0000

    feat: simplify app

--- app/app.py
+++ app/app.py
@@ -2,31 +2,10 @@
 # coding: utf-8
 
 from dalle_mini.backend import ServiceError, get_images_from_backend
-
 import streamlit as st
 
-# streamlit.session_state is not available in Huggingface spaces.
-# Session state hack https://huggingface.slack.com/archives/C025LJDP962/p1626527367443200?thread_ts=1626525999.440500&cid=C025LJDP962
-
-from streamlit.report_thread import get_report_ctx
-def query_cache(q_emb=None):
-    ctx = get_report_ctx()
-    session_id = ctx.session_id
-    session = st.server.server.Server.get_current()._get_session_info(session_id).session
-    if not hasattr(session, "_query_state"):
-        setattr(session, "_query_state", q_emb)
-    if q_emb:
-        session._query_state = q_emb
-    return session._query_state
-
-def set_run_again(state):
-    query_cache(state)
-
-def should_run_again():
-    state = query_cache()
-    return state if state is not None else False
-
-st.sidebar.markdown("""
+st.sidebar.markdown(
+    """
 <style>
 .aligncenter {
     text-align: center;
@@ -35,8 +14,11 @@ st.sidebar.markdown("""
 <p class="aligncenter">
     <img src="https://raw.githubusercontent.com/borisdayma/dalle-mini/main/img/logo.png"/>
 </p>
-""", unsafe_allow_html=True)
-st.sidebar.markdown("""
+""",
+    unsafe_allow_html=True,
+)
+st.sidebar.markdown(
+    """
 ___
 <p style='text-align: center'>
 DALL·E mini is an AI model that generates images from any prompt you give!
@@ -47,21 +29,20 @@ Created by Boris Dayma et al. 2021
 <br/>
 <a href="https://github.com/borisdayma/dalle-mini" target="_blank">GitHub</a> | <a href="https://wandb.ai/dalle-mini/dalle-mini/reports/DALL-E-mini--Vmlldzo4NjIxODA" target="_blank">Project Report</a>
 </p>
-        """, unsafe_allow_html=True)
+        """,
+    unsafe_allow_html=True,
+)
 
-st.header('DALL·E mini')
-st.subheader('Generate images from text')
+st.header("DALL·E mini")
+st.subheader("Generate images from text")
 
 prompt = st.text_input("What do you want to see?")
 
-test = st.empty()
 DEBUG = False
-if prompt != "" or (should_run_again and prompt != ""):
+if prompt != "":
     container = st.empty()
-    # The following mimics `streamlit.info()`.
-    # I tried to get the secondary background color using `components.streamlit.config.get_options_for_section("theme")["secondaryBackgroundColor"]`
-    # but it returns None.
-    container.markdown(f"""
+    container.markdown(
+        f"""
         <style> p {{ margin:0 }} div {{ margin:0 }} </style>
         <div data-stale="false" class="element-container css-1e5imcs e1tzin5v1">
         <div class="stAlert">
@@ -78,32 +59,39 @@ if prompt != "" or (should_run_again and prompt != ""):
         </div>
         </div>
         <small><i>Predictions may take up to 40s under high load. Please stand by.</i></small>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
     try:
         backend_url = st.secrets["BACKEND_SERVER"]
         print(f"Getting selections: {prompt}")
         selected = get_images_from_backend(prompt, backend_url)
 
-        cols = st.columns(4)
+        margin = 0.1  # for better position of zoom in arrow
+        n_columns = 3
+        cols = st.columns([1] + [margin, 1] * (n_columns - 1))
         for i, img in enumerate(selected):
-            cols[i%4].image(img)
-
+            cols[(i % n_columns) * 2].image(img)
         container.markdown(f"**{prompt}**")
-        
-        set_run_again(st.button('Again!', key='again_button'))
-    
+
+        st.button("Again!", key="again_button")
+
     except ServiceError as error:
         container.text(f"Service unavailable, status: {error.status_code}")
     except KeyError:
         if DEBUG:
-            container.markdown("""
+            container.markdown(
+                """
             **Error: BACKEND_SERVER unset**
 
             Please, create a file called `.streamlit/secrets.toml` inside the app's folder and include a line to configure the server URL:
             ```
             BACKEND_SERVER="<server url>"
             ```
-            """)
+            """
+            )
         else:
-            container.markdown('Error -5, please try again or [report it](mailto:pcuenca-dalle@guenever.net).')
+            container.markdown(
+                "Error -5, please try again or [report it](mailto:pcuenca-dalle@guenever.net)."
+            )
