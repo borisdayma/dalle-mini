605df32c2fa28db256ff9fbbd219e0b8ad666cbd
Boris Dayma <boris.dayma@gmail.com>
2022-01-27 02:55:52 +0000

    style(tokenizer): remove unused variables

--- src/dalle_mini/model/tokenizer.py
+++ src/dalle_mini/model/tokenizer.py
@@ -1,10 +1,7 @@
 """ DalleBart tokenizer """
 from transformers import BartTokenizerFast
-from transformers.utils import logging
 
 from .utils import PretrainedFromWandbMixin
-
-logger = logging.get_logger(__name__)
 
 
 class DalleBartTokenizer(PretrainedFromWandbMixin, BartTokenizerFast):
