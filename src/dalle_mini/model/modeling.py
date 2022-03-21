45c12cb983894b719853dce895f64676bc90969d
Boris Dayma <boris.dayma@gmail.com>
2022-03-21 17:47:53 +0000

    feat: add layernorm at last encoder layer

--- src/dalle_mini/model/modeling.py
+++ src/dalle_mini/model/modeling.py
@@ -26,7 +26,7 @@ import jax
 import jax.numpy as jnp
 import msgpack.exceptions
 from flax.core.frozen_dict import unfreeze
-from flax.linen import make_causal_mask, combine_masks
+from flax.linen import combine_masks, make_causal_mask
 from flax.linen.attention import dot_product_attention_weights
 from flax.serialization import from_bytes
 from flax.traverse_util import flatten_dict, unflatten_dict
@@ -43,14 +43,16 @@ from transformers.file_utils import (
 )
 from transformers.generation_flax_utils import FlaxSampleOutput
 from transformers.modeling_flax_outputs import (
+    FlaxBaseModelOutput,
+    FlaxBaseModelOutputWithPastAndCrossAttentions,
     FlaxCausalLMOutputWithCrossAttentions,
     FlaxSeq2SeqLMOutput,
-    FlaxBaseModelOutputWithPastAndCrossAttentions,
 )
 from transformers.modeling_flax_utils import ACT2FN
 from transformers.models.bart.modeling_flax_bart import (
     FlaxBartAttention,
-    FlaxBartEncoderLayerCollection,
+    FlaxBartDecoder,
+    FlaxBartEncoder,
     FlaxBartForConditionalGeneration,
     FlaxBartForConditionalGenerationModule,
     FlaxBartModule,
@@ -388,23 +390,6 @@ def createFlaxBartEncoderLayer(do_remat=False):
     return FlaxBartEncoderLayer
 
 
-class FlaxBartEncoderLayerCollection(FlaxBartEncoderLayerCollection):
-    """
-    Edits:
-    - use custom FlaxBartEncoderLayer
-    - allow Gradient Checkpointing (nn.remat)
-    """
-
-    def setup(self):
-        self.layers = [
-            createFlaxBartEncoderLayer(self.config.gradient_checkpointing)(
-                self.config, name=str(i), dtype=self.dtype
-            )
-            for i in range(self.config.encoder_layers)
-        ]
-        self.layerdrop = self.config.encoder_layerdrop
-
-
 def createFlaxBartDecoderLayer(do_remat=False):
 
     if do_remat:
@@ -523,6 +508,75 @@ def createFlaxBartDecoderLayer(do_remat=False):
             return outputs
 
     return FlaxBartDecoderLayer
+
+
+class FlaxBartEncoderLayerCollection(nn.Module):
+    config: DalleBartConfig
+    dtype: jnp.dtype = jnp.float32  # the dtype of the computation
+    """
+    Edits:
+    - use custom FlaxBartEncoderLayer
+    - allow Gradient Checkpointing (nn.remat)
+    """
+
+    def setup(self):
+        self.layers = [
+            createFlaxBartEncoderLayer(self.config.gradient_checkpointing)(
+                self.config, name=str(i), dtype=self.dtype
+            )
+            for i in range(self.config.encoder_layers)
+        ]
+        self.final_layernorm = nn.LayerNorm(dtype=self.dtype, epsilon=1e-05)
+
+    def __call__(
+        self,
+        hidden_states,
+        attention_mask,
+        deterministic: bool = True,
+        output_attentions: bool = False,
+        output_hidden_states: bool = False,
+        return_dict: bool = True,
+    ):
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+
+        n_layers = len(self.layers)
+        for i, encoder_layer in enumerate(self.layers):
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+            layer_outputs = encoder_layer(
+                hidden_states,
+                attention_mask=attention_mask,
+                output_attentions=output_attentions,
+                deterministic=deterministic,
+            )
+
+            hidden_states = layer_outputs[0]
+            # final layernorm on the output of the last layer
+            if i == n_layers - 1:
+                if self.config.ln_positions in ["normformer", "swinv2"]:
+                    hidden_states = self.final_layernorm(hidden_states)
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+
+        # add hidden states from the last layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        outputs = [
+            hidden_states,
+            all_hidden_states,
+            all_self_attns,
+        ]
+
+        if not return_dict:
+            return tuple(v for v in outputs if v is not None)
+
+        return FlaxBaseModelOutput(
+            last_hidden_state=hidden_states,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attns,
+        )
 
 
 class FlaxBartDecoderLayerCollection(nn.Module):
