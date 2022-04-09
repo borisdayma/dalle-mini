8553be2178ef0afdd1519c63e700f4d4cbba24d9
Boris Dayma <boris.dayma@gmail.com>
2022-04-09 21:38:09 +0000

    feat: define exact scanned layers

--- src/dalle_mini/model/partitions.py
+++ src/dalle_mini/model/partitions.py
@@ -67,7 +67,10 @@ def set_partitions(in_dict, use_scan):
     if use_scan:
         # add None dimension to layers
         result = {
-            k: (P(*(None,) + v) if v is not None else None) for k, v in result.items()
+            k: (P(*(None,) + v) if v is not None else None)
+            if any(x in k for x in ["FlaxBartEncoderLayers", "FlaxBartDecoderLayers"])
+            else v
+            for k, v in result.items()
         }
     assert _unmatched not in result.values(), "Incomplete partition spec."
     return freeze(unflatten_dict(result))
