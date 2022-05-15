6fb7ce5990082fbff5112d43b48d9a36446afe68
Boris Dayma <boris.dayma@gmail.com>
2022-05-15 20:02:40 +0000

    feat: update layout

--- app/gradio/app.py
+++ app/gradio/app.py
@@ -5,7 +5,7 @@ import os
 import gradio as gr
 from backend import get_images_from_backend
 
-block = gr.Blocks()
+block = gr.Blocks(css=".container { max-width: 800px; margin: auto; }")
 backend_url = os.environ["BACKEND_SERVER"] + "/generate"
 
 
@@ -19,20 +19,34 @@ with block:
     gr.Markdown(
         "DALL·E mini is an AI model that generates images from any prompt you give!"
     )
-    prompt = gr.inputs.Textbox(
-        placeholder="An astronaut riding a horse in a photorealistic style"
-    )
-    text_run = gr.Button("Run")
-    result = gr.Gallery()
-    text_run.click(infer, inputs=prompt, outputs=result)
+    with gr.Group():
+        with gr.Box():
+            with gr.Row().style(mobile_collapse=False, equal_height=True):
+
+                text = gr.Textbox(
+                    label="Enter your prompt", show_label=False, max_lines=1
+                ).style(
+                    border=(True, False, True, True),
+                    margin=False,
+                    rounded=(True, False, False, True),
+                    container=False,
+                )
+                btn = gr.Button("Run").style(
+                    margin=False,
+                    rounded=(False, True, True, False),
+                )
+        gallery = gr.Gallery(label="Generated images", show_label=False).style(
+            grid=[3], height="auto"
+        )
+        btn.click(infer, inputs=text, outputs=gallery)
 
     gr.Markdown(
         """___
-<p style='text-align: center'>
-Created by Boris Dayma et al. 2021-2022
-<br/>
-<a href="https://github.com/borisdayma/dalle-mini" target="_blank">GitHub</a> | <a href="https://wandb.ai/dalle-mini/dalle-mini/reports/DALL-E-mini--Vmlldzo4NjIxODA" target="_blank">Project Report</a>
-</p>"""
+   <p style='text-align: center'>
+   Created by Boris Dayma et al. 2021-2022
+   <br/>
+   <a href="https://github.com/borisdayma/dalle-mini" target="_blank">GitHub</a> | <a href="https://wandb.ai/dalle-mini/dalle-mini/reports/DALL-E-mini--Vmlldzo4NjIxODA" target="_blank">Project Report</a>
+   </p>"""
     )
 
 
