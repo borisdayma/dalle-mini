466a6b628e3783065e26f1e719395c5fe230bf00
Madison Swain-Bowden <bowdenm@spu.edu>
2022-06-10 22:34:54 -0700

    Add --rm flag to default docker run command (#222)

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -2,4 +2,4 @@
 
 # This script is used to run the docker image. Change or remove GPU flag if you dont have nvidia-docker or the needed GPUs
 current_dir=$(pwd)
-docker run -it -p 8888:8888  --gpus all  -v $current_dir:/workspace dalle-mini:latest
+docker run --rm -it -p 8888:8888  --gpus all  -v $current_dir:/workspace dalle-mini:latest
