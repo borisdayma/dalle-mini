f62e0545636130e78925cf417ff2b2be66a07e17
Patrick Berlatto Piccini <66441004+patrickpiccini@users.noreply.github.com>
2022-06-11 10:02:21 -0300

    right annotation of "PWD" (#226)

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # This script is used to run the docker image. Change or remove GPU flag if you dont have nvidia-docker or the needed GPUs
-current_dir=$(pwd)
-docker run --rm -it -p 8888:8888  --gpus all  -v $current_dir:/workspace dalle-mini:latest
+docker run --rm -it -p 8888:8888  --gpus all  -v "${PWD}":/workspace dalle-mini:latest
