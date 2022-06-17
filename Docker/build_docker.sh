ad50f3452d284806b434b5aac201005e4043f41f
Hayley Davies <cd230099@falmouth.ac.uk>
2022-06-17 05:53:14 +0100

    Now works based on flags passed to build_docker.sh
    Added --cpu --gpu and --auto flags to build_docker.sh
    
    These then get passed to the Dockerfile so the docker file knows to download:
    --cpu: jaxlib with nocuda
    --gpu: jaxlib with cuda11
    --auto: a version of jax which is found via a directory input into pip install

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -1,1 +1,10 @@
-docker build . -t dalle-mini:latest
+DEVICE=auto
+
+case "$1" in
+--cpu) DEVICE=cpu ;;
+--gpu) DEVICE=gpu ;;
+--auto) DEVICE=auto ;;
+esac
+shift
+
+docker build . -t dalle-mini:latest --build-arg device_type=$DEVICE
