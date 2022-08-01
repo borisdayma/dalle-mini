fix: swin v2 scaling (thanks @lucidrains)

src/dalle_mini/model/modeling.py
@@ -188,6 +188,7 @@ def dot_product_attention_weights(
     precision: PrecisionLike = None,
     sinkhorn_iters: int = 1,
     causal: bool = False,
+    tau = 1.
 ):
     """
     Computes dot-product attention weights given query and key.
@@ -214,6 +215,9 @@ def dot_product_attention_weights(
     if embed_pos is not None:
         attn_weights = attn_weights + embed_pos
 
+    # divide by tau (used in Swin v2)
+    attn_weights = attn_weights / jnp.maximum(tau, 0.01)
+    
     # normalize the attention weights
     if causal or sinkhorn_iters == 1:
         # sinkhorn does not work for causal (leaks info of future tokens into past)
@@ -424,6 +428,7 @@ class FlaxBartAttention(FlaxBartAttention):
         else:
             embed_pos = None
 
+        tau = self.tau if self.config.use_cosine_attention else 1.
         attn_weights = dot_product_attention_weights(
             query_states,
             key_states,
@@ -438,10 +443,8 @@ class FlaxBartAttention(FlaxBartAttention):
             precision=None,
             sinkhorn_iters=self.config.sinkhorn_iters,
             causal=self.causal,
+            tau = tau
         )
-        if self.config.use_cosine_attention:
-            # divide by tau
-            attn_weights = attn_weights / jnp.maximum(self.tau, 0.01)
 
         attn_output = jnp.einsum("...hqk,...khd->...qhd", attn_weights, value_states)
         if self.config.use_head_scale: